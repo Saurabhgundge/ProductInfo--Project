f51ff723d8d5ecf9774ef1dcd3648beb74aefcbc
saurabh <saurabhgundge@gmail.com>
2023-04-12 12:09:53 +0530

    my 1st commit

---
+++ ProductDetailProject/Models/CategoryTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProductDetailProject.Models
+{
+
+    [Table("CategoryTable")]
+    public class CategoryTable
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        
+        public int? ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual ProductTable ProductTable { get; set; }
+    }
+}
